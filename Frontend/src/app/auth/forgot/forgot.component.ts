21a28b1eb9988078d581fedd3f828ec740997bdc
unknown <dav.randhir@gmail.com>
2019-09-12 00:43:30 +0530

    changed validation

--- Frontend/src/app/auth/forgot/forgot.component.ts
+++ Frontend/src/app/auth/forgot/forgot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot.component.css']
 })
 export class ForgotComponent implements OnInit {
-
-  constructor() { }
+resetForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.resetForm = this.fb.group({
+email: ['', [Validators.required, Validators.email, Validators.pattern(
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+)]]
+    });
   }
 
 }
