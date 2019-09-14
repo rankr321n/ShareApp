95d46ea08ae00aaac91b9b68c0d9c3ecceeda448
Randhir Sah <dav.randhir@gmail.com>
2019-09-14 19:27:42 +0530

    sendgrid

--- Backend/src/server/server.js
+++ Backend/src/server/server.js
@@ -3,8 +3,7 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 const router = express.Router();
-const dotenv = require("dotenv");
-dotenv.config();
+
 var port = 3000;
 var authenticationControl = require("../auth/auth-server");
 // var registerationControl = require('../register/register-controler')
